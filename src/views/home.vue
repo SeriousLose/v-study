614e5532bcfc2c0fc575ddd577cb26c5ffdddf81
helongfei <643756132@qq.com>
2021-07-08 17:42:46 +0800

    <feature>  增加esline

---
+++ src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+
+</style>

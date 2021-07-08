117db16d9bedbc5c6f1af7fa1784e357d3ffaba7
helongfei <643756132@qq.com>
2021-07-08 16:25:11 +0800

    <feature> 配置vue3.0项目结构

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import store from './store'
 
-createApp(App).mount('#app')
+
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app');
---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+  // { path: '/', name: 'Home', component: Home },
+  // { path: '/vuex', name: 'Vuex', component: Vuex },
+  // {
+  //   path: '/axios',
+  //   name: 'Axios',
+  //   component: () => import('@/views/axios.vue'), // 懒加载组件
+  // },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;

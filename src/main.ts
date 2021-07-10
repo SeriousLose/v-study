0865896e6cd3ec76350916b4fde6a9f30c1f18b6
helongfei <643756132@qq.com>
2021-07-11 00:08:12 +0800

    <feature> 测试提交

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,5 @@ import router from './router/index'
 import store from './store'
 
 createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
+
+console.log( '11') ;
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,12 +1,12 @@
-import Axios from 'axios'
-import { ElMessage } from 'element-plus'
+import Axios from 'axios';
+import { ElMessage } from 'element-plus';
 
 const baseURL = 'https://api.github.com'
 
 const axios = Axios.create({
   baseURL,
   timeout: 20000 // 请求超时 20s
-})
+}) ;
 
 // 前置拦截器（发起请求之前的拦截）
 axios.interceptors.request.use(
